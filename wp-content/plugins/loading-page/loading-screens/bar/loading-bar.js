7d0f9c5ecdc3afe97389523864d035762bb849c2
Janis <janis@js0.ch>
2021-07-15 20:03:40 +0200

    Update

--- wp-content/plugins/loading-page/loading-screens/bar/loading-bar.js
+++ wp-content/plugins/loading-page/loading-screens/bar/loading-bar.js
@@ -1,14 +1,14 @@
 var cp_loadingpage = cp_loadingpage || {};
 cp_loadingpage.graphics = cp_loadingpage.graphics || {};
 
-cp_loadingpage.graphics['logo'] = {
+cp_loadingpage.graphics['bar'] = {
 	created: false,
-	attr   : {percentage:0},
+	attr   : {},
 	create : function(opt){
 		opt.backgroundColor = opt.backgroundColor || "#000000";
+		opt.height          = opt.height || 1;
 		opt.foregroundColor = opt.foregroundColor || "#FFFFFF";
 
-		this.attr['foreground'] = opt.foregroundColor;
 		var css_o = {
 			width: "100%",
 			height: "100%",
@@ -40,56 +40,48 @@ cp_loadingpage.graphics['logo'] = {
 
 		this.attr['overlay'] = jQuery("<div class='lp-screen'></div>").css(css_o).appendTo("html");
 
+		this.attr['bar'] = jQuery("<div class='lp-screen-graphic'></div>").css({
+			height: opt.height+"px",
+			marginTop: "-" + (opt.height / 2) + "px",
+			backgroundColor: opt.foregroundColor,
+			width: "0%",
+			position: "absolute",
+			top: "50%"
+		}).appendTo(this.attr['overlay']);
+
 		if (opt.text) {
 			this.attr['text'] = jQuery("<div class='lp-screen-text'></div>").text("0%").css({
-				lineHeight: "40px",
 				height: "40px",
 				width: "100px",
 				position: "absolute",
-				fontSize: "30px",
-				top: this.attr['overlay'].height()/2,
-				left: this.attr['overlay'].width()/2-50,
+				fontSize: "3em",
+				top: "50%",
+				left: "50%",
+				marginTop: "-" + (59 + opt.height) + "px",
 				textAlign: "center",
+				marginLeft: "-50px",
 				color: opt.foregroundColor
 			}).appendTo(this.attr['overlay']);
 		}
 
-		if(
-			typeof opt[ 'lp_ls' ]  != 'undefined'
-		)
-		{
-			if(
-				typeof opt[ 'lp_ls' ][ 'logo' ]  != 'undefined'  &&
-				typeof opt[ 'lp_ls' ][ 'logo' ][ 'image' ]  != 'undefined'  &&
-				!/^\s*$/.test( opt[ 'lp_ls' ][ 'logo' ][ 'image' ]  )
-			)
-			{
-				var me 	= this,
-					wrapper = jQuery('<span style="width:120px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);display: inline-block;"></span>'),
-					img_url = jQuery.trim( opt[ 'lp_ls' ][ 'logo' ][ 'image' ] ),
-					img = jQuery('<img src="'+img_url+'"  style="cursor:pointer;width:120px;" />');
-				img.on('click',cp_loadingpage.destroyLoader);
-				wrapper.append(img).appendTo( me.attr[ 'overlay' ] );
-				if(me.attr[ 'text' ])
-					wrapper.append(me.attr[ 'text' ].css({'position':'relative', 'top':'auto', 'left':'auto', 'width':'100%', 'margin-top':'20px'}));
-			}
-		}
-		this.set(0);
 		this.created = true;
 	},
 
 	set : function(percentage){
-		this.attr['percentage'] = percentage;
+		this.attr['bar'].stop().animate({
+			width: percentage + "%",
+			minWidth: percentage + "%"
+		}, 200);
+
 		if (this.attr['text']) {
 			this.attr['text'].text(Math.ceil(percentage) + "%");
 		}
-		jQuery( '#lp_ls_cover' ).css( { 'width': (100-percentage)+'%', 'right':0 } );
 	},
 
 	complete : function(callback){
 		callback();
 		var me = this;
-		this.attr['overlay'].fadeOut(200, function () {
+		this.attr['overlay'].fadeOut(500, function () {
 			me.attr['overlay'].remove();
 		});
 	}
